8637838ecb6d3ebad6eac1406725d0145674ed0f
emmanuelologe <epicraez@gmail.com>
2023-04-22 16:24:33 +0100

    Prep 1 and Prep 2 assignments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write ("what is your first name?");
+        string first_name = Console.ReadLine();
+        Console.Write ("what is your last name?");
+        string last_name = Console.ReadLine();
+        Console.WriteLine ($"Your full name {last_name}, {first_name} {last_name}");
     }
 }
+
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string percentage = Console.ReadLine();
+        int number = int.Parse(percentage);
+
+        if (number >= 90)
+        {
+            Console.WriteLine("Your Grade is A");
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("Your Grade is B");
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine("Your Grade is C");
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine("Your Grade is D");
+        }
+        else if (number <= 60)
+        {
+            Console.Write("Your Grade is F");
+        }
+
+        if (number >= 70)
+        {
+            Console.Write("Congratulations you have passed!");
+        }
+        else
+        {
+            Console.Write("You can do better, You did not pass.");
+        }
     }
 }
