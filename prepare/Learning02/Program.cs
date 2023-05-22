d6a2bca87f8bf9e4d9adefa524d06bb31e212889
emmanuelologe <epicraez@gmail.com>
2023-05-23 00:50:11 +0100

    week 3

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Web designer";
+        job1._company = "SiteCliq";
+        job1._startYear = 2023;
+        job1._endYear   =2028;
+
+        Job job2 = new Job();
+        job2._jobTitle = "CEO";
+        job2._company = "My Billion Dollaars Company";
+        job2._startYear = 2028;
+        job2._endYear = 3000;
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Emmanuel";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+
+        myResume.Display();
     }
 }
